fa8582252d102732bbfd6338cd195a993a28462a
Johannes7k75 <57235955+Johannes7k75@users.noreply.github.com>
2022-01-26 21:43:29 +0100

    updates

--- index.js
+++ index.js
@@ -1,21 +1,6 @@
-document.getElementById("submit").onclick = function () {
-    location.reload()
-    
-    // const response = await fetch("https://reqbin.com/echo/post/json", {
-    // method: 'POST',
-    // headers: {
-    //   'Accept': 'application/json',
-    //   'Content-Type': 'application/json'
-    // },
-    // body: `{
-    //    "Id": 78912,
-    //    "Customer": "Jason Sweet",
-    //    "Quantity": 1,
-    //    "Price": 18.00
-    //   }`,
-    // });
-    
-    // response.json().then(data => {
-    //   console.log(data);
-    // });
+function myFunction() {
+    // location.reload()
+    // fetch("https://hcaptcha.com/siteverify")
+    alert()
+    console.log(FormData["h-captcha-response"])
 }
