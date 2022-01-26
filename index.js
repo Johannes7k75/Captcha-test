1ae5969a7545ee7e62cce4391c13b59d207bb36f
Johannes7k75 <57235955+Johannes7k75@users.noreply.github.com>
2022-01-26 21:04:51 +0100

    let

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-SECRET_KEY = "dfb2e89e-71d1-4b28-aa5a-e9d38b1cc84d"    // replace with your secret key
-VERIFY_URL = "https://hcaptcha.com/siteverify"
-token = request.POST_DATA['h-captcha-response']
-data = { 'secret': SECRET_KEY, 'response': token }
-response = http.post(url=VERIFY_URL, data=data)
-response_json = JSON.parse(response.content)
-success = response_json['success']
+let SECRET_KEY = "dfb2e89e-71d1-4b28-aa5a-e9d38b1cc84d"    // replace with your secret key
+let VERIFY_URL = "https://hcaptcha.com/siteverify"
+let token = request.POST_DATA['h-captcha-response']
+let data = { 'secret': SECRET_KEY, 'response': token }
+let response = http.post(url=VERIFY_URL, data=data)
+let response_json = JSON.parse(response.content)
+let success = response_json['success']
 
+alert(success)
 
 // const response = await fetch("https://reqbin.com/echo/post/json", {
 // method: 'POST',
