314a26a71a04ff8ab9f31dd2f510f557f1c4108e
Johannes7k75 <57235955+Johannes7k75@users.noreply.github.com>
2022-01-26 21:18:21 +0100

    main

--- index.js
+++ index.js
@@ -1,15 +1,5 @@
 document.getElementById("submit").onclick = function () {
-    let SECRET_KEY = "dfb2e89e-71d1-4b28-aa5a-e9d38b1cc84d"    // replace with your secret key
-    let VERIFY_URL = "https://hcaptcha.com/siteverify"
-    let token = request.POST_DATA['h-captcha-response']
-    let data = { 'secret': SECRET_KEY, 'response': token }
-    let response = http.post(url=VERIFY_URL, data=data)
-    let response_json = JSON.parse(response.content)
-    let success = response_json['success']
-    
-    alert(success)
-    
-    // location.reload()
+    location.reload()
     
     // const response = await fetch("https://reqbin.com/echo/post/json", {
     // method: 'POST',
